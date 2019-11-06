c0b7963ae57dd0182eb83ad7246b14c2e103760a
Nolan Chapelle <53437031+nchapelle@users.noreply.github.com>
2019-11-06 14:58:22 -0500

    i had res.json instead of res.sendFile

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -4,7 +4,7 @@
 // These data sources hold arrays of information on table-data, waitinglist, etc.
 // ===============================================================================
 
-var friendData = require("../app/data/friends");
+var friendData = require("../data/friends");
 
 
 // ===============================================================================
--- app/routes/htmlRoutes.js
+++ app/routes/htmlRoutes.js
@@ -15,7 +15,7 @@ module.exports = function(app) {
   // In each of the below cases the user is shown an HTML page of content
   // ---------------------------------------------------------------------------
   app.get("/", function(req, res) {
-    res.json(path.join(__dirname, "../public/home.html"));
+    res.sendFile(path.join(__dirname, "../public/home.html"));
   });
 
 
