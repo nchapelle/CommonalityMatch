aeddf4054db69d0255b3d9e7a137fdc6697b9ee9
Nolan Chapelle <53437031+nchapelle@users.noreply.github.com>
2019-11-06 14:46:42 -0500

    fixed api routes hopefull

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -15,17 +15,17 @@ module.exports = function(app) {
   // In each of the below cases the user is shown an HTML page of content
   // ---------------------------------------------------------------------------
   app.get("/", function(req, res) {
-    res.json(path.join(__dirname, "../app/public/home.html"));
+    res.json(path.join(__dirname, "app/public/home.html"));
   });
 
 
   app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "../app/public/survey.html"));
+    res.sendFile(path.join(__dirname, "app/public/survey.html"));
   });
 
 
   // If no matching route is found default to home
   app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "../app/public/home.html"));
+    res.sendFile(path.join(__dirname, "app/public/home.html"));
   });
 };
