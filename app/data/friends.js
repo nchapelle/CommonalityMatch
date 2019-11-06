661a20a78d1d19cca49ceeabac46064834454795
Nolan Chapelle <53437031+nchapelle@users.noreply.github.com>
2019-11-06 14:36:42 -0500

    all routes pages and apis are ready for deployment

---
+++ app/data/friends.js
@@ -0,0 +1,38 @@
+
+let friendArr = [
+    {
+        "name": "Mr. Disagreeable",
+        "photo": "https://careers.workopolis.com/wp-content/uploads/sites/5/2013/06/fe5d1a8b4eeca6c44a081c25f3ec.jpg",
+        "scores": [
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1"
+        ]
+    },
+    {
+        "name": "Mr. Agreeable",
+        "photo": "https://www.psychologistworld.com/images/articles/a/575x360-v-as-67207980.jpg",
+        "scores": [
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5"
+        ]
+    }
+
+]
+
+module.exports = friendArr
---
+++ routes/apiRoutes.js
@@ -0,0 +1,50 @@
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// These data sources hold arrays of information on table-data, waitinglist, etc.
+// ===============================================================================
+
+var friendData = require("../app/data/friends");
+
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+  // API GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases when a user visits a link
+  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+  // ---------------------------------------------------------------------------
+
+  app.get("/api/friends", function(req, res) {
+    res.json(friendData);
+  });
+
+
+  // API POST Requests
+  // Below code handles when a user submits a form and thus submits data to the server.
+  // In each of the below cases, when a user submits form data (a JSON object)
+  // ...the JSON is pushed to the appropriate JavaScript array
+  // (ex. User fills out a reservation request... this data is then sent to the server...
+  // Then the server saves the data to the tableData array)
+  // ---------------------------------------------------------------------------
+
+  app.post("/api/friends", function(req, res) {
+    // req.body is available since we're using the body parsing middleware
+      friendData.push(req.body); 
+  });
+
+  // ---------------------------------------------------------------------------
+  // I added this below code so you could clear out the table while working with the functionality.
+  // Don"t worry about it!
+
+//   app.post("/api/clear", function(req, res) {
+//     // Empty out the arrays of data
+//     friendData.length = 0;
+//     waitListData.length = 0;
+
+//     res.json({ ok: true });
+//   });
+};
